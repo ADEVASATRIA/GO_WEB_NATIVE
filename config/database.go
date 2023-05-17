effd88e4132d033601b068ec38e3b155b63f326e
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-17 21:28:25 +0700

    (On Going)Menambahkan connect ke database xampp dan berhasil membuat tabel

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "root:@/go_product")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go_product")
 	if err != nil {
 		panic(err)
 	}
