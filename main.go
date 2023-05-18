89d813779f9637b14923ed64d736ea012b412bc4
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-18 18:52:04 +0700

    (Done)Membuat Menu Homepage

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"GO-WEB-NATIVE/config"
+	"GO-WEB-NATIVE/controllers/homecontroller"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config.ConnectDB()
+
+
+	//1.homepage
+	http.HandleFunc("/", homecontroller.Welcome)
 	log.Println("Server running into port 8080")
 	http.ListenAndServe(":8080", nil)
 }
