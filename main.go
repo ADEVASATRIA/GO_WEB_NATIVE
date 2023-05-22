167918201caffc3353155698f3422be30f257c16
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-22 20:42:33 +0700

    (Add)menambahkan controllers/categorycontroller

--- main.go
+++ main.go
@@ -3,22 +3,12 @@ package main
 import (
 	"GO-WEB-NATIVE/config"
 	"GO-WEB-NATIVE/controllers/homecontroller"
+	"GO-WEB-NATIVE/controllers/categorycontroller"
 	"log"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
-// func main() {
-// 	config.ConnectDB()
-
-// 	//1.homepage
-// 	http.HandleFunc("/", homecontroller.Welcome)
-
-// 	//2. Categories
-
-//		log.Println("Server running into port 8080")
-//		http.ListenAndServe(":8080", nil)
-//	}
 func main() {
 	config.ConnectDB()
 	r := gin.Default()
@@ -27,7 +17,19 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		homecontroller.Welcome(c.Writer, c.Request)
 	})
-
+	// 2. Categories route
+	r.GET("/categories", func(c *gin.Context) {
+		categorycontroller.Index(c.Writer, c.Request)
+	})
+	r.GET("/categories/add", func(c *gin.Context) {
+		categorycontroller.Add(c.Writer, c.Request)
+	})
+	r.GET("/categories/edit", func(c *gin.Context) {
+		categorycontroller.Edit(c.Writer, c.Request)
+	})
+	r.GET("/categories/delete", func(c *gin.Context) {
+		categorycontroller.Delete(c.Writer, c.Request)
+	})
 	// 3. /ping route from the first code snippet
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
