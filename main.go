2e99525832b6724300e165a3bce277a7d0b5761c
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-22 20:22:25 +0700

    (Import Module)Menambahkan Module Gin untuk melakukan refresh debug

--- main.go
+++ main.go
@@ -5,14 +5,36 @@ import (
 	"GO-WEB-NATIVE/controllers/homecontroller"
 	"log"
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
+// func main() {
+// 	config.ConnectDB()
+
+// 	//1.homepage
+// 	http.HandleFunc("/", homecontroller.Welcome)
+
+// 	//2. Categories
+
+//		log.Println("Server running into port 8080")
+//		http.ListenAndServe(":8080", nil)
+//	}
 func main() {
 	config.ConnectDB()
+	r := gin.Default()
 
+	// 1. Homepage route
+	r.GET("/", func(c *gin.Context) {
+		homecontroller.Welcome(c.Writer, c.Request)
+	})
 
-	//1.homepage
-	http.HandleFunc("/", homecontroller.Welcome)
-	log.Println("Server running into port 8080")
-	http.ListenAndServe(":8080", nil)
+	// 3. /ping route from the first code snippet
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Println("Server running on port 8080")
+	r.Run(":8080")
 }
