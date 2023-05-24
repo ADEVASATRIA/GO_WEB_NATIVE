be27ab406632777438ef56417da023b6f7f16591
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-24 21:26:44 +0700

    (Add)Menambahkan Function Index pada category controller

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,19 +1,30 @@
 package categorycontroller
 
-import(
-	
+import (
+	"GO-WEB-NATIVE/models/categorymodel"
 	"net/http"
+	"text/template"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	
+	categories := categorymodel.GetAll()
+	data := map[string]any{
+		"categories": categories,
+	}
+
+	temp, err := template.ParseFiles("views/category/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	temp.Execute(w, data)
 }
 func Add(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 func Edit(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 func Delete(w http.ResponseWriter, r *http.Request) {
-	
+
 }
